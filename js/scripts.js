d31003bc4fe44ec0721277ec56fb676631f0520f
David Mortkowitz and Meria Thomas <meria@>
2018-08-07 13:30:41 -0700

    Add Javascript and jQuery to show and hide blocks of text upon mouse click.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".initially-hidden").toggle().fadeIn();
+  });
+  $(".clickable2").click(function() {
+    $(".hiddentext2").toggle();
+  });
+  $(".clickable3").click(function() {
+    $(".hiddentext3").toggle();
+  });
+  $(".clickable4").click(function() {
+    $(".hiddentext4").toggle();
+  });
+  $(".clickable5").click(function() {
+    $(".hiddentext5").toggle();
+  });
+  $(".clickable6").click(function() {
+    $(".hiddentext6").toggle();
+  });
+  $(".clickable7").click(function() {
+    $(".hiddentext7").toggle();
+  });
+
+
+});
