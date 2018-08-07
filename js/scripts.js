94bbb911c739906fc8ad03241321eaecb60be34e
David Mortkowitz and Meria Thomas <david@>
2018-08-07 16:07:10 -0700

    Add efficient jQuery coding, streamline HTML.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,9 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $(".initially-hidden").toggle().fadeIn();
-  });
-  $(".clickable2").click(function() {
-    $(".hiddentext2").toggle();
-  });
-  $(".clickable3").click(function() {
-    $(".hiddentext3").toggle();
-  });
-  $(".clickable4").click(function() {
-    $(".hiddentext4").toggle();
-  });
-  $(".clickable5").click(function() {
-    $(".hiddentext5").toggle();
-  });
-  $(".clickable6").click(function() {
-    $(".hiddentext6").toggle();
-  });
-  $(".clickable7").click(function() {
-    $(".hiddentext7").toggle();
-  });
+    $(".initially-hidden").fadeIn();
+});
+  $(".well",this).click(function() {
+    $(".hiddentext2",this).toggle();
 
-
+  });
 });
